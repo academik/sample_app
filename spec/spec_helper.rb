66cb146db97821d6e8a50f8940f9e2eb1f179ccf
Andrew <andrewdkippen@gmail.com>
2013-11-09 10:10:32 -0500

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  # added based on RailsTutorial 3.10 
+  config.include Capybara::DSL
 end
